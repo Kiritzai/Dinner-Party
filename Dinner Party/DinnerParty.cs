e63d713b3033377dcb36ce46d7b2dc6ea85d5ba5
Roemy <toonen.remy@gmail.com>
2014-11-18 15:54:36 +0100

    Dinner Party

---
+++ Dinner Party/DinnerParty.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinner_Party
+{
+    class DinnerParty
+    {
+        private const int CostOfFoodPerPerson = 25;
+        private int NumberOfPeople;
+        private decimal CostOfBeveragesPerPerson;
+        private decimal CostOfDecorations;
+
+        public void SetPartyOptions(int people, bool fancy)
+        {
+            NumberOfPeople = people;
+            CalculateCostOfDecorations(fancy);
+        }
+
+        public int GetNumberOfPeople()
+        {
+            return NumberOfPeople;
+        }
+
+        public void SetHealthyOption(bool HealthOption)
+        {
+            if (HealthOption)
+            {
+                CostOfBeveragesPerPerson = 5.00M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.00M;
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            if (fancy)
+            {
+                CostOfDecorations = (NumberOfPeople * 15.00M) + 50.00M;
+            }
+            else
+            {
+                CostOfDecorations = (NumberOfPeople * 7.50M) + 30.00M;
+            }
+        }
+
+        public decimal CalculateCost()
+        {
+            decimal subtotal = ((NumberOfPeople * CostOfBeveragesPerPerson) + CostOfFoodPerPerson) + CostOfDecorations;
+            decimal total = subtotal - (subtotal / 100 * 5);
+            return total;
+        }
+    }
+}
---
+++ Dinner Party/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dinner_Party
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerparty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dinnerparty = new DinnerParty(); // { NumberOfPeople = 5 };
+            dinnerparty.SetPartyOptions(5, checkBox1.Checked);
+            numericUpDown1.Value = dinnerparty.GetNumberOfPeople();
+            dinnerparty.SetHealthyOption(checkBox2.Checked);
+            //dinnerparty.CalculateCostOfDecorations(true);
+            DisplayDinnerPartyCost();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerparty.SetPartyOptions((int)numericUpDown1.Value, checkBox1.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerparty.CalculateCostOfDecorations(checkBox1.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerparty.SetHealthyOption(checkBox2.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerparty.CalculateCost();
+            labelCost.Text = Cost.ToString("c");
+        }
+    }
+}
